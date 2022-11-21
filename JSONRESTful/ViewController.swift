d18ce8d43d53819f913b2f2734b0b6840380c9e6
JeampierCM <jeampier.cornejo@tecsup.edu.pe>
2022-11-21 11:19:19 -0800

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by jeampier on 11/21/22.
+//  Copyright © 2022 miempresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
